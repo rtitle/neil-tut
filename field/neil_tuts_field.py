c266fbebdcdefbe18a3c90d3c1d857a166b01eb9
Robert Title <rtitle@broadinstitute.org>
2020-04-21 22:02:28 -0400

    Add items you can buy

--- field/neil_tuts_field.py
+++ field/neil_tuts_field.py
@@ -23,12 +23,12 @@ class Player:
         self.pos = self.image_standing.get_rect().move(200, Player.ground - 150)
         self.total_fall = 0
 
-    def draw(self, direction, on_ladder, screen):
-        if direction < 0:
+    def draw(self, direction, on_ladder, modal_active, screen):
+        if direction < 0 and not modal_active:
             screen.blit(self.image_walking_left, self.pos)
-        elif direction > 0:
+        elif direction > 0 and not modal_active:
             screen.blit(self.image_walking_right, self.pos)
-        elif on_ladder:
+        elif on_ladder and not modal_active:
             screen.blit(self.image_climbing, self.pos)
         else:
             screen.blit(self.image_standing, self.pos)
@@ -52,18 +52,21 @@ class Player:
             self.pos.bottom = fall_to
             self.total_fall = 0
 
-    # TODO fix
+    def is_on_object(self, pos, frame=None):
+        arr = self.camera.adjust(pos)
+        if frame:
+            arr = list(filter(lambda x: x[0] == frame, arr))
+        return any(self.pos.colliderect(a[1]) for a in arr)
+
     def is_on_ladder(self, ladder):
-        arr = self.camera.adjust(ladder.pos)
-        return any(self.pos.colliderect(l[1].inflate(-50, -50)) for l in arr)
+        return self.is_on_object(ladder.pos.inflate(-50, -50))
 
     def is_above_platform(self, platform):
         arr = self.camera.adjust(platform.pos)
         return any(self.pos.right > p[1].left+10 and self.pos.left < p[1].right-10 and self.pos.bottom < p[1].bottom - 30 for p in arr)
 
     def is_on_coin(self, coin, frame):
-        arr = list(filter(lambda x: x[0] == frame, self.camera.adjust(coin.pos)))
-        return any(self.pos.colliderect(c[1].inflate(-10, -10)) for c in arr)
+        return self.is_on_object(coin.pos.inflate(-10, -10), frame)
 
     def should_die(self):
         return self.total_fall >= 200
@@ -78,6 +81,16 @@ class GameObject:
             if not frame or frame == o[0]:
                 screen.blit(self.image, o[1])
 
+class BuyObject(GameObject):
+    def __init__(self, pos, name, item_name, price):
+        super().__init__(pos, name)
+        self.price = price
+        self.item_name = item_name
+        self.active = True
+
+    def buy(self):
+        self.active = False
+
 class Camera:
     def __init__(self, background, speed):
         self.speed = speed
@@ -89,9 +102,12 @@ class Camera:
         if self.pos.left > 800:
             self.pos.left = 0
             self.frame += 1
+            return True
         elif self.pos.left < 0:
             self.pos.left = 800
             self.frame -= 1
+            return True
+        return False
 
     def adjust(self, rect):
         arr = [
@@ -100,6 +116,9 @@ class Camera:
           (self.frame + 1, rect.move(-self.pos.left + 800, 0))
         ]
         return list(filter(lambda x: x[1].left < 800 and x[1].right > 0, arr))
+
+    def visible_frames(self):
+        return [self.frame - 1, self.frame, self.frame + 1]
 
 class Score:
     def __init__(self, font_name, font_size):
@@ -112,6 +131,11 @@ class Score:
 
     def update(self):
         self.score = self.score + 1
+        msg = "Coins: %d" % self.score
+        self.image = self.font.render(msg, 0, pg.Color("Yellow"))
+
+    def subtract(self, amount):
+        self.score -= amount
         msg = "Coins: %d" % self.score
         self.image = self.font.render(msg, 0, pg.Color("Yellow"))
 
@@ -141,8 +165,76 @@ class Health:
         for i in range(self.health):
             screen.blit(self.image, self.pos.move(i*50, 0))
 
+# TODO: modal class hierarchy
 
-# quick function to load an image
+class OkModal:
+    def __init__(self, pos, font_name, font_size, msg):
+        self.pos = pos
+        self.active = False
+        font = pg.font.Font(font_name, font_size)
+        font.set_bold(1)
+
+        self.message_image = font.render(msg, 0, pg.Color("Yellow"))
+        self.message_pos = self.message_image.get_rect().move(pos.left + 20, pos.top + 20)
+
+        self.ok_image = font.render("OK", 0, pg.Color("Yellow"))
+        self.ok_pos = self.ok_image.get_rect().move(pos.left + 210, pos.bottom - 50)
+
+    def set_active(self):
+        self.active = True
+
+    def dismiss(self): 
+        self.active = False
+
+    def is_in_ok(self, x, y):
+        return self.ok_pos.inflate(20, 20).collidepoint(x, y)
+
+    def draw(self, screen):
+        pg.draw.rect(screen, pg.Color("Blue"), self.pos)
+        screen.blit(self.message_image, self.message_pos)
+        pg.draw.rect(screen, pg.Color(173, 216, 230), self.ok_pos.inflate(20, 20))
+        screen.blit(self.ok_image, self.ok_pos)
+
+class YesNoModal:
+    def __init__(self, pos, font_name, font_size):
+        self.pos = pos
+        self.active = False
+        self.buy_object = None
+        
+        self.font = pg.font.Font(font_name, font_size)
+        self.font.set_bold(1)
+
+        self.yes_image = self.font.render("Yes", 0, pg.Color("Yellow"))
+        self.yes_pos = self.yes_image.get_rect().move(pos.left + 50, pos.bottom - 50)
+
+        self.no_image = self.font.render("No", 0, pg.Color("Yellow"))
+        self.no_pos = self.no_image.get_rect().move(pos.right - 90, pos.bottom - 50)
+
+    def set_active(self, buy_object):
+        self.active = True
+        self.buy_object = buy_object
+
+    def dismiss(self):
+        self.active = False
+
+    def set_message(self, msg):
+        self.message_image = self.font.render(msg, 0, pg.Color("Yellow"))
+        self.message_pos = self.message_image.get_rect().move(self.pos.left + 20, self.pos.top + 20)
+
+    def is_in_yes(self, x, y):
+        return self.yes_pos.inflate(20, 20).collidepoint(x, y)
+    
+    def is_in_no(self, x, y):
+        return self.no_pos.inflate(20, 20).collidepoint(x, y)
+
+    def draw(self, screen):
+        pg.draw.rect(screen, pg.Color("Blue"), self.pos)
+        screen.blit(self.message_image, self.message_pos)
+        pg.draw.rect(screen, pg.Color(173, 216, 230), self.yes_pos.inflate(20, 20))
+        screen.blit(self.yes_image, self.yes_pos)
+        pg.draw.rect(screen, pg.Color(173, 216, 230), self.no_pos.inflate(20, 20))
+        screen.blit(self.no_image, self.no_pos)
+
 def load_image(name, width, height):
     path = os.path.join(main_dir, "data", name)
     img = pg.image.load(path).convert()
@@ -191,39 +283,52 @@ def main():
     background = GameObject(pg.Rect(0, 0, 800, 600), "green_hills_1.png")
     ladder = GameObject(pg.Rect(400, 280, 100, 250), "ladder3.gif")
     platform = GameObject(pg.Rect(500, 245, 250, 160), "green_hills_platform.gif")
-    coins = {}
+    hammer = BuyObject(pg.Rect(700, 460, 50, 50), "hammer.png", "hammer", 10)
     game_objects = [background, ladder, platform]
+    coins = {}
+    buy_objects = {1: [hammer]}
 
     camera = Camera(background, 10)
     player = Player("netut_standing.gif", "netut_walking_left.gif", "netut_walking_right.gif", "netut_climbing.gif", 5, 20, camera)
     score = Score(None, 40)
     health = Health(3, "netut_heart.gif")
+    modal = YesNoModal(pg.Rect(150, 100, 450, 200), None, 40)
+    no_money_modal = OkModal(pg.Rect(150, 100, 500, 150), None, 40, "You don't have enough coins.")
     game_over = EndScreen(None, 60, "Game Over")
     you_win = EndScreen(None, 60, "You Win!")
 
     while health.health > 0 and score.score < 100:
+        click = False
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
             if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
                 return
+            if event.type == pg.MOUSEBUTTONDOWN:
+                click = True
         keystate = pg.key.get_pressed()
+        mouse_pos = pg.mouse.get_pos()
         direction = keystate[pg.K_RIGHT] - keystate[pg.K_LEFT]
         climb_direction = keystate[pg.K_DOWN] - keystate[pg.K_UP]
 
-        camera.move(direction)
+        # move the camera
+        if not modal.active and not no_money_modal.active:
+            if camera.move(direction):
+                # reset the buy modal so it pops up again
+                modal.buy_object = None
+
+        # display each static game object, including the background
         for g in game_objects:
             g.draw(camera, screen)
 
-        score.draw(screen)
-        health.draw(screen)
-
-        # TODO clean up coin logic
-        for f in [camera.frame - 1, camera.frame, camera.frame + 1]:
-            if f not in coins.keys():
+        # display each stateful game object
+        for f in camera.visible_frames():
+            # populate infinite coins
+            if f not in coins:
                 coins[f] = []
                 for i in range(5):
                     coins[f].append(GameObject(pg.Rect(500 + (i * 50), 255, 40, 40), "netut_coin.gif"))
+            # display coins
             for c in coins[f]:
                 if player.is_on_coin(c, f):
                     if pg.mixer:
@@ -232,19 +337,55 @@ def main():
                     score.update()
                 else:
                     c.draw(camera, screen, f)
+            # display buy objects
+            if f in buy_objects:
+                for o in buy_objects[f]:
+                    if o.active:
+                        o.draw(camera, screen, f)
+                        if not modal.active and modal.buy_object != o and player.is_on_object(o.pos, f):
+                            modal.set_active(o)
 
+        # display buy modal
+        if modal.active and modal.buy_object:
+            modal.set_message(f"Buy {modal.buy_object.item_name} for {modal.buy_object.price} coins?")
+            modal.draw(screen)
+            if click:
+                if modal.is_in_yes(mouse_pos[0], mouse_pos[1]):
+                    modal.dismiss()
+                    if modal.buy_object.price <= score.score:
+                        modal.buy_object.buy()
+                        score.subtract(modal.buy_object.price)
+                    else:
+                        no_money_modal.set_active()
+                if modal.is_in_no(mouse_pos[0], mouse_pos[1]):
+                    modal.dismiss()
+
+        # display "not enough money" modal
+        if no_money_modal.active:
+            no_money_modal.draw(screen)
+            if click:
+                if no_money_modal.is_in_ok(mouse_pos[0], mouse_pos[1]):
+                    no_money_modal.dismiss()
+
+        # make the player climb or fall
         on_ladder = player.is_on_ladder(ladder)
         if on_ladder:
             player.climb(climb_direction)
         else:
             player.fall(platform)
 
+        # make the player die
         if player.should_die():
             health.lose_health()
             if pg.mixer:
                 grunt_sound.play()
 
-        player.draw(direction, on_ladder, screen)
+        # draw the player
+        player.draw(direction, on_ladder, modal.active or no_money_modal.active, screen)
+
+        # draw the score and health
+        score.draw(screen)
+        health.draw(screen)
 
         pg.display.update()
         clock.tick(40)
